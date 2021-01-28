71a045183cd6eff769026c294895f56a6bcd986b
SivaramKrishnan12 <74295183+SivaramKrishnan12@users.noreply.github.com>
2021-01-28 21:03:45 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,92 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import numpy as np
+from flask import Flask, request, jsonify, render_template, json, Response, redirect, flash
+import pickle
+from config import Config
+from forms import Pedestrian_prediction_Form 
+from datetime import datetime, timedelta
+from sklearn.preprocessing import StandardScaler
 
-model = pickle.load(open('iri.pkl', 'rb'))
+# model = pickle.load(open('iri.pkl', 'rb'))
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def man():
+#     return render_template('home.html')
 
 
-@app.route('/')
-def man():
-    return render_template('home.html')
+# @app.route('/predict', methods=['POST'])
+# def home():
+#     data1 = request.form['a']
+#     data2 = request.form['b']
+#     data3 = request.form['c']
+#     data4 = request.form['d']
+#     arr = np.array([[data1, data2, data3, data4]])
+#     pred = model.predict(arr)
+#     return render_template('after.html', data=pred)
 
 
-@app.route('/predict', methods=['POST'])
-def home():
-    data1 = request.form['a']
-    data2 = request.form['b']
-    data3 = request.form['c']
-    data4 = request.form['d']
-    arr = np.array([[data1, data2, data3, data4]])
-    pred = model.predict(arr)
-    return render_template('after.html', data=pred)
+
+
+
+
+# app = Flask(__name__)
+# app.config.from_object(Config)
+#model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route("/")
+@app.route("/index")
+@app.route("/home")
+def index():
+    return render_template("index.html", index = True)
+
+@app.route("/Pedestrian_forecast")
+def Pedestrian_forecast():
+    return render_template("Pedestrian_forecast.html", Pedestrian_forecast = True)
+
+@app.route("/Pedestrian_prediction", methods=['GET','POST'])
+def Pedestrian_prediction():
+    form = Pedestrian_prediction_Form()
+
+    #using standard scaler
+
+    #scaler = StandardScaler()
+
+    if form.is_submitted():
+
+        return render_template("user.html", result = request.form)
+            
+        date = form.date.data
+        year = date.dt.year
+        day_of_year1 = date.dt.dayofyear
+        monthly_index = date.dt.month
+        day_of_week = date.dt.dayofweek + 1
+        rainfall = form.rainfall.data
+        solar_exposure = form.solar_exposure.data
+        restriction = form.restriction.data
+        public_holiday = form.public_holiday.data
+        minimum_temperature = form.minimum_temperature.data
+        maximum_temperature = form.maximum_temperature.data
+
+        
+    return render_template( "Pedestrian_prediction.html",title = "Pedestrian prediction",  form = form,  Pedestrian_prediction = True)
+
+
+@app.route("/contributions")
+def contributions():
+
+    return render_template("contributions.html", contributions = True)
+
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port = 8080)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
 
 
 
